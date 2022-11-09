0515dfd2a09ba45121740858d18fe5410ef43a28
Anthony Allois <anthonyallois@gmail.com>
2022-11-09 09:24:26 -0300

    Primer Commit

---
+++ app.js
@@ -0,0 +1,19 @@
+
+const express = require('express')
+
+
+const app = express()
+const PORT = process.env.PORT || 8080;
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {  
+  res.render('home');
+})
+
+app.listen(PORT, () => console.log(`Server Up on port ${PORT}`))
