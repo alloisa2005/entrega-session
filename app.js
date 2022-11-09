2680dbe98a2bc866e7e4617efb56dd460e39ed12
Anthony Allois <anthonyallois@gmail.com>
2022-11-09 10:33:09 -0300

    Ajustes en EJS

--- app.js
+++ app.js
@@ -2,16 +2,29 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const session = require('express-session')
+const MongoDBSession = require('connect-mongodb-session')(session)
 const UserModel = require('./models/User.model')
 
 const app = express()
 const PORT = process.env.PORT || 8080;
+const mongoUri = 'mongodb://localhost:27017/aae-sessions';
 
-app.use(session({
-  key: 'user_uid',
-  secret: 'mi secreto',
+///////////////// Conexión MONGO DB /////////////////////////////////
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,   
+  useUnifiedTopology: true
+}).then( res => console.log('Base de datos conectada!!'))
+/////////////////////////////////////////////////////////////////////
+const store = new MongoDBSession({
+  uri: mongoUri,
+  collection: 'mySessions'
+})
+
+app.use(session({  
+  secret: 'mi palabra secreta',
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,  
+  store: store,
   cookie: { maxAge: 60000 }
 }))
 
@@ -22,15 +35,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(__dirname + '/public'));
 
-//////// Conexión MONGO DB ////////////////////////////////////////
-mongoose.connect('mongodb://localhost:27017/aae-sessions', {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
-}).then( res => console.log('Base de datos conectada!!'))
-///////////////////////////////////////////////////////////////////
 
-app.get('/', (req, res) => {      
-  res.render('home');
+
+app.get('/', (req, res) => {     
+  res.redirect('/login');  
 })
 
 app.get('/dashboard', (req, res) => {  
