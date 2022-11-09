c1116db5ecc348627eb3eac628d3b0b2067126ba
Anthony Allois <anthonyallois@gmail.com>
2022-11-09 10:06:01 -0300

    Agregando sessions

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 
 const express = require('express')
 const mongoose = require('mongoose')
+const session = require('express-session')
 const UserModel = require('./models/User.model')
 
 const app = express()
 const PORT = process.env.PORT || 8080;
+
+app.use(session({
+  key: 'user_uid',
+  secret: 'mi secreto',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
@@ -20,8 +29,24 @@ mongoose.connect('mongodb://localhost:27017/aae-sessions', {
 }).then( res => console.log('Base de datos conectada!!'))
 ///////////////////////////////////////////////////////////////////
 
-app.get('/', (req, res) => {  
-  res.render('home',{msg: 'Holaaaaa'});
+app.get('/', (req, res) => {      
+  res.render('home');
+})
+
+app.get('/dashboard', (req, res) => {  
+  res.render('dashboard');
+})
+
+app.get('/register', (req, res) => {  
+  res.render('register');
+})
+
+app.get('/login', (req, res) => {  
+  res.render('login');
+})
+
+app.get('/register', (req, res) => {  
+  res.render('logout');
 })
 
 app.listen(PORT, () => console.log(`Server Up on port ${PORT}`))
